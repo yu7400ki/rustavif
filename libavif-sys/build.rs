cd6290eb21e3cab5bb5bedc382e7c0167b2766b8
yu7400ki <yu7400ki@gmail.com>
2025-08-21 00:33:12 +0900

    support rav1e codec

--- libavif-sys/build.rs
+++ libavif-sys/build.rs
@@ -1,3 +1,27 @@
+/*
+Copyright 2020 Charles Samuels <ks@ks.ax>, Paolo Barbolini <paolo@paolo565.org>
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 use std::env;
 use std::path::PathBuf;
 
@@ -51,14 +75,29 @@ fn main() {
             "OFF"
         },
     );
-    cfg.define(
-        "AVIF_CODEC_RAV1E",
-        if cfg!(feature = "codec-rav1e") {
-            "LOCAL"
-        } else {
-            "OFF"
-        },
-    );
+
+    #[cfg(feature = "codec-rav1e")]
+    {
+        use std::fs;
+
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let rav1e_inc = out_dir.join("include").join("rav1e");
+        fs::create_dir_all(&rav1e_inc).expect("mkdir rav1e include dir failed");
+
+        let crate_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        fs::copy(crate_dir.join("rav1e.h"), rav1e_inc.join("rav1e.h"))
+            .expect("copy rav1e.h failed");
+
+        cfg.define("AVIF_CODEC_RAV1E", "SYSTEM")
+            .define("AVIF_CODEC_LIBRARIES", "rav1e")
+            .define("RAV1E_INCLUDE_DIR", &rav1e_inc)
+            .define("RAV1E_LIBRARY", "-rav1e");
+    }
+    #[cfg(not(feature = "codec-rav1e"))]
+    {
+        cfg.define("AVIF_CODEC_RAV1E", "OFF");
+    }
+
     cfg.define(
         "AVIF_CODEC_SVT",
         if cfg!(feature = "codec-svt") {
--- libavif-sys/src/lib.rs
+++ libavif-sys/src/lib.rs
@@ -2,6 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(feature = "codec-rav1e")]
+pub use rav1e::capi::*;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
